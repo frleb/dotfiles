ff56a4755b0dd695d8198041fbec78ec18284e50
Frédéric Lebel <flebel@opval.com>
2024-09-22 16:16:35 -0400

    configuration de wezterm

---
+++ home/dot_config/wezterm/wezterm.lua
@@ -0,0 +1,14 @@
+local wezterm = require 'wezterm'
+
+local config = wezterm.config_builder()
+
+config.color_scheme = 'Solarized Dark Higher Contrast (Gogh)'
+
+-- config.font = wezterm.font 'JetBrains Mono'
+-- config.font = wezterm.font('JetBrainsMono Nerd Font')
+-- config.font = wezterm.font('FiraCode Nerd Font Mono')
+config.font = wezterm.font('Fira Code')
+
+config.hide_tab_bar_if_only_one_tab = true
+
+return config
