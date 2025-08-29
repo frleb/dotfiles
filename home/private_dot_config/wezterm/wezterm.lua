c7b9176d71c5b57b836d33254390da7defe16f49
Frédéric Lebel <flebel@opval.com>
2025-08-29 19:30:29 -0400

    feature: forcer la taille de la font dans wezterm à 15

--- home/private_dot_config/wezterm/wezterm.lua
+++ home/private_dot_config/wezterm/wezterm.lua
@@ -8,7 +8,7 @@ config.color_scheme = 'Solarized Dark Higher Contrast (Gogh)'
 -- config.font = wezterm.font('JetBrainsMono Nerd Font')
 -- config.font = wezterm.font('FiraCode Nerd Font Mono')
 config.font = wezterm.font('Fira Code')
-
+config.font_size = 15
 config.hide_tab_bar_if_only_one_tab = true
 
 return config
